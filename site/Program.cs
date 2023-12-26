c827d974ecd48d6d71d3a1bdeac81d79310f32c5
Cyrus <88629892+RefGnom@users.noreply.github.com>
2023-12-27 01:38:05 +0500

    Created authorization on swagger

--- site/Program.cs
+++ site/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using PUFAMI_Project.Data;
 
@@ -16,14 +19,45 @@ namespace PUFAMI_Project
                 options.UseMySql(connectingString, ServerVersion.AutoDetect(connectingString));
             });
 
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", configure => { })
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, configure => { });
+
+            builder.Services.AddAuthorization(configure =>
+            {
+                configure.AddPolicy("BasicAuthentication",
+                    new AuthorizationPolicyBuilder("BasicAuthentication")
+                    .RequireAuthenticatedUser()
+                    .Build());
+            });
+
+            builder.Services.AddControllers();
+
+
+            //builder.Services.AddAuthorization(configure =>
+            //{
+            //    configure.
+            //});
+
             var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             DefaultFilesOptions options = new();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("/html/main_page.html");
-
+            options.DefaultFileNames.Add("/html/welcome_page.html");
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapControllers();
 
             app.Run(async (context) => await context.Response.WriteAsync("Hello World"));
 
