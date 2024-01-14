bed2152cb6fadb64767be259998de9a298a8b2a6
RedStrikeRF <shubin152@gmail.com>
2024-01-15 00:39:13 +0500

    upd

--- site/wwwroot/JS/addclass.js
+++ site/wwwroot/JS/addclass.js
@@ -4,17 +4,23 @@ document.querySelector('.form').addEventListener('submit', function(event) {
   // Получаем значения из полей формы
   const className = document.querySelector('input[name="class_name"]').value;
   const classData = document.querySelector('input[name="class"]').value;
-
+  const user = localStorage.getItem("PUFAMIUser");
+  const classes = JSON.stringify({
+    [id]:
+      {
+        "name": className,
+        "graduate": classData,
+        "owner": Object.keys(user)[0],
+        "structure" : {}
+      }
+    });
   // Настройка запроса
   const requestOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-       class_name: className,
-       class: classData 
-      })
+    body: classes
   };
 
   // Отправка запроса на сервер
@@ -26,7 +32,7 @@ document.querySelector('.form').addEventListener('submit', function(event) {
       return response.json();
     })
     .then(data => {
-      let classes = localStorage.getItem('PUFAMIUserClass');
+      let classes = localStorage.setItem('PUFAMIUserClass', classes);
     })
     .catch(error => {
       // Обработка ошибок
