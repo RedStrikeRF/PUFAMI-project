426ef356c9108a7428f4408b2fdfca5591fd3e80
Kdzefuf <115563854+Kdzefuf@users.noreply.github.com>
2024-01-15 11:05:40 +0500

    upd

--- site/obj/Debug/net8.0/PUFAMI_Project.AssemblyInfo.cs
+++ site/obj/Debug/net8.0/PUFAMI_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PUFAMI_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+318e38fa4a09bfaf8c8f0cae48125685d9675574")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d7e73f7b67d88bff28f6c46ad922b012b2f669")]
 [assembly: System.Reflection.AssemblyProductAttribute("PUFAMI_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PUFAMI_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
